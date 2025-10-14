4f9a94557696918881503ffb493ea2bdb33bb409
Linda Jensen <linda.jensen81@me.com>
2025-10-14 16:58:17 +0200

    style(hero): add span for styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,9 @@ function Hero() {
     <div className="hero">
       <img className="hero__image" src="/src/assets/img/portrait.png" alt="" />
       <div className="hero__content">
-        <h1 className="hero__title">Hello, I'm Linda</h1>
+        <h1 className="hero__title">
+          Hello, I'm <span>Linda</span>
+        </h1>
         <p className="hero__text">
           Tech-loving teacher that's trying to trade in her whiteboard for a
           keyboard
