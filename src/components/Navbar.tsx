891857cb01232eb268d1342b5f2c5b700348db45
Linda Jensen <linda.jensen81@me.com>
2025-10-14 08:52:24 +0200

    create Navbar component and import in App

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
