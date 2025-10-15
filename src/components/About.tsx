8520b734fdc72b90c3c5b2a41f72458dd53c381a
Linda Jensen <linda.jensen81@me.com>
2025-10-15 08:39:27 +0200

    add About component and initial styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,36 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <section className="about">
+      <h2 className="about__title">
+        About <span className="about--highlight">& Skills</span>{" "}
+      </h2>
+      <p className="about__text">
+        Highly motivated Frontend Developer student in the midst of a career
+        jump. Even though I haven't worked in the field yet I'm hoping to bring
+        my creative mindset and problem-solving skills into the world of coding.
+        I'm committed to continuous learning and growth and can't wait to apply
+        everything I've learned in a professional setting. Based in Varberg,
+        Sweden. To learn more about my education and work history, check out my{" "}
+        <a className="about__resume" href="resume.pdf" target="_blank">
+          resume
+        </a>
+        . Thanks for stopping by!
+      </p>
+
+      <div className="about__skills">
+        <div className="about__skill about__skill--html">HTML</div>
+        <div className="about__skill about__skill--css">CSS</div>
+        <div className="about__skill about__skill--js">JS</div>
+        <div className="about__skill about__skill--vue">Vue</div>
+        <div className="about__skill about__skill--react">React</div>
+        <div className="about__skill about__skill--ts">TS</div>
+        <div className="about__skill about__skill--uxui">UX/UI</div>
+        <div className="about__skill about__skill--git">Git</div>
+        <div className="about__skill about__skill--express">Express</div>
+        <div className="about__skill about__skill--postgres">Postgres</div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
